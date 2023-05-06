d290b2ac5cb07dc56e9704e491c76d0ca32e8238
vitharanaps <120036474+vitharanaps@users.noreply.github.com>
2023-05-06 23:13:56 +0530

    not Found
    Create Not Found Image

--- src/screens/notFound/notFound.jsx
+++ src/screens/notFound/notFound.jsx
@@ -1,8 +1,15 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>notFound</div>
+    <Box sx={{ display:"flex", alignItems:"center", justifyContent:"center", width:"100%", marginTop:10, height:"100%", flexDirection:"column"}}>
+       <img src='../assests/stop.png' />
+    <Typography variant='h1'color="error"  >Page Not Found</Typography>
+    <Typography variant='h1'color="primary"  >404</Typography>
+
+    </Box>
+   
   )
 }
 
